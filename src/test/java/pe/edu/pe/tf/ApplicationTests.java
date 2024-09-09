855141f4bce711e49bffe113963e98b11d910ab8
DMal31 <u202216039@upc.edu.pe>
2024-09-08 23:41:44 -0500

    Semana 4
    Avance Querys

---
+++ src/test/java/pe/edu/pe/tf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.tf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
